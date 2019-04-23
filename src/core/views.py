1187ff39c6d9d188d8c8632279930d194cf7afa2
subajat1 <bayumunajat@outlook.com>
2019-04-23 20:26:38 +0800

    fix filter implementation

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.db.models import Q
 from django.shortcuts import render
 
 from .models import Journal
@@ -10,6 +11,18 @@ def BootstrapFilterView(request):
     title_contains_query = request.GET.get('title_contains')
     title_exact_query = request.GET.get('title_exact')
     title_or_author_query = request.GET.get('title_or_author')
+
+    if not title_contains_query in (None, ''):
+        qs = qs.filter(title__icontains=title_contains_query)
+
+    elif not title_exact_query in (None, ''):
+        qs = qs.filter(title__iexact=title_exact_query)
+
+    elif not title_or_author_query in (None, ''):
+        qs = qs.filter( Q(title__icontains=title_or_author_query) 
+                      | Q(author__name__icontains=title_contains_query)
+                      ).distinct()
+
     context = {
         'queryset': qs
     }

58b92192e89b2d4ad48f13379f6386495a86dc9b
subajat1 <bayumunajat@outlook.com>
2019-04-23 18:22:30 +0800

    init core models

---
+++ src/core/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class Journal(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(Author, related_name='journal_author', on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    reviewed = models.BooleanField(default=False)

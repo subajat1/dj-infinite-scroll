98e5c3f6f55869341a90a2ff158d20af500a2adc
subajat1 <bayumunajat@outlook.com>
2019-04-23 20:15:42 +0800

    fix search form and backend respond

--- src/core/models.py
+++ src/core/models.py
@@ -23,3 +23,7 @@ class Journal(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
     reviewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Journal
+
 def BootstrapFilterView(request):
-    return render(request, 'bootstrap_form.html', {})
+    qs = Journal.objects.all()
+
+    title_contains_query = request.GET.get('title_contains')
+    title_exact_query = request.GET.get('title_exact')
+    title_or_author_query = request.GET.get('title_or_author')
+    context = {
+        'queryset': qs
+    }
+
+    return render(request, 'bootstrap_form.html', context)
